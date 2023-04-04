49d5ad01fad2dd71dd916d8ea9f3aeee416cdbad
Abhay Kushwah <67948225+Abhaykushwah@users.noreply.github.com>
2023-04-04 20:18:21 +0530

    db_connect updated -- search feature added

--- db_connect.py
+++ db_connect.py
@@ -10,19 +10,27 @@ else:
 ## setting up cursor to run commands
 cursor = conn.cursor()
 
-# ## temp
-site = input("Enter the site name for which you're saving the password : ")
-password_hash = input("Enter password hash to save in database : \n")
-note = input("Any additnoal NOTE[Default:NONE] : " or "NONE")
+# ## user input
+# site = input("Enter the site name for which you're saving the password : ")
+# password_hash = input("Enter password hash to save in database : \n")
+# note = input("Any additnoal NOTE[Default:NONE] : " or "NONE")
 
 
 ## Command Execution 
 # cursor.execute('''insert into passwords values("test.com","9q1sr21ba5b28408cef4f84ea3591f8","same as above");''')
-cursor.execute(f"insert into passwords values('{site}', '{password_hash}', '{note}');")
-conn.commit()
-cursor.execute('''select * from passwords;''')
+# cursor.execute(f"insert into passwords values('{site}', '{password_hash}', '{note}');")
+# conn.commit()
+
+
+## search query
+search = input("Enter key phrase : ")
+cursor.execute(f'''select site_name, passwd from passwords where site_name like '%{search}%' or site_name like '%{search}' or site_name like '{search}%' or site_name like '{search}' or note like '%{search}%' or note like '%{search}' or note like '{search}%' or note like '{search}';''')
+# cursor.execute('''select site_name, passwd from passwords where site_name like '%google%' or site_name like '%google' or site_name like 'google%' or site_name like 'google' or note like '%google%' or note like '%google' or note like 'google%';''')
 # cursor.execute("desc passwords;")
 
+
+# fetch all
+# cursor.execute("select * from passwords;")
 
 data = cursor.fetchall()
 # print data
